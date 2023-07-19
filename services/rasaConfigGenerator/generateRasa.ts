ac97a43f3a084395b8f789353f9fbe0e44be6159
hansel <hansel@openb.net>
2023-07-19 13:28:48 +0800

    Update generateRasa.ts
    Refactor the code in generateRasa.ts to use a relative path for copying the templates. This will ensure that the correct templates are copied to the rasaPath directory. Additionally, update the train command to include the fixed model name and add a step to transfer the trained model to a remote server.
    
    The changes made in this commit will improve the reliability and efficiency of the rasaConfigGenerator.

--- services/rasaConfigGenerator/generateRasa.ts
+++ services/rasaConfigGenerator/generateRasa.ts
@@ -6,7 +6,7 @@ function main() {
     const rasaPath = basePath + "/rasa"
     console.log("basePath",basePath)
     generateRasaConfig(rasaPath, Customer)
-    shell.exec(`cp -R ${rasaPath}/templates/rasa/ ${rasaPath}`)
+    shell.exec(`cp -R ./templates/rasa/ ${rasaPath}`)
     shell.exec(`docker run -v ${rasaPath}:/app -v ${basePath}/models:/app/models rasa/rasa:3.5.6-full train --fixed-model-name ${Customer.id}-${Customer.version}`)
     shell.exec(`cd ${basePath}/generated/models/ && scp ${Customer.id}-${Customer.version}.tar.gz eighty@192.168.0.151:/data/rasa/models/`)
 }
