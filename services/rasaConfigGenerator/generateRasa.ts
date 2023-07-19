a0827b5355e322baa0f7d7c3e1e34c7f4b79ae4a
hansel <hansel@openb.net>
2023-07-19 13:59:46 +0800

    Update generateRasa.ts
    The generateRasa.ts file has been updated to include a new shell command to list the contents of the rasaPath directory after copying the templates. Additionally, another shell command has been added to list the contents of the basePath directory in order to get the base models. Finally, a shell command has been added to copy the generated model to a remote server.

--- services/rasaConfigGenerator/generateRasa.ts
+++ services/rasaConfigGenerator/generateRasa.ts
@@ -8,8 +8,9 @@ function main() {
     generateRasaConfig(rasaPath, Customer)
     shell.exec(`ls -lah ${rasaPath}`)
     shell.exec(`cp -R ./templates/rasa/ ${rasaPath}`)
+    shell.exec(`ls -lah ${rasaPath}`)
     shell.exec(`docker run -v ${rasaPath}:/app -v ${basePath}/models:/app/models rasa/rasa:3.5.6-full train --fixed-model-name ${Customer.id}-${Customer.version}`)
-    shell.exec(`echo "trying to get base models" && ls -lah && ls -lah ${basePath}/models`)
+    shell.exec(`echo "trying to get base models" && ls -lah && ls -lah ${basePath}`)
     shell.exec(`cd ${basePath}/models/ && scp ${Customer.id}-${Customer.version}.tar.gz eighty@192.168.0.151:/data/rasa/models/`)
 }
 main()
