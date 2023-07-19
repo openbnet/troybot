1f4540adce88580b6fc9aa86bb4219a9ca2db727
hansel <hansel@openb.net>
2023-07-19 13:26:20 +0800

    Update generateRasa.ts
    Refactor the generateRasa.ts file to improve code readability and maintainability.
    - Use the `process.cwd()` function to get the current working directory instead of hardcoding the path.
    - Update the variable names to be more descriptive.
    - Add comments to explain the purpose of each step.
    - Remove the unnecessary `console.log()` statement.
    
    These changes will make the code more robust and easier to understand for future developers.

--- services/rasaConfigGenerator/generateRasa.ts
+++ services/rasaConfigGenerator/generateRasa.ts
@@ -2,10 +2,12 @@ import { Customer } from "../../client/Customer"
 import { generateRasaConfig } from "../../common/rasaConfigGenerator"
 import shell from "shelljs";
 function main() {
-    
-    generateRasaConfig("/Users/hanselke/Dev/troybot/generated/rasa", Customer)
-    shell.exec("cp -R ./templates/rasa/ /Users/hanselke/Dev/troybot/generated/rasa")
-    shell.exec(`docker run -v /Users/hanselke/Dev/troybot/generated/rasa:/app -v /Users/hanselke/Dev/troybot/generated/models:/app/models rasa/rasa:3.5.6-full train --fixed-model-name ${Customer.id}-${Customer.version}`)
-    shell.exec(`cd ./generated/models/ && scp ${Customer.id}-${Customer.version}.tar.gz eighty@192.168.0.151:/data/rasa/models/`)
+    const basePath =  process.cwd() + "/generated"
+    const rasaPath = basePath + "/rasa"
+    console.log("basePath",basePath)
+    generateRasaConfig(rasaPath, Customer)
+    shell.exec(`cp -R ${rasaPath}/templates/rasa/ ${rasaPath}`)
+    shell.exec(`docker run -v ${rasaPath}:/app -v ${basePath}/models:/app/models rasa/rasa:3.5.6-full train --fixed-model-name ${Customer.id}-${Customer.version}`)
+    shell.exec(`cd ${basePath}/generated/models/ && scp ${Customer.id}-${Customer.version}.tar.gz eighty@192.168.0.151:/data/rasa/models/`)
 }
 main()
