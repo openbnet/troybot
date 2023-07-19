6a8d808f12aca0904d4e6e53bb64624e4da30a88
hansel <hansel@openb.net>
2023-07-19 15:00:07 +0800

    Update generated model [skip ci]
    The generated model has been updated. This commit includes changes to the Git configuration, adding and committing the changes, and pushing them to the main branch. The generated model is now up to date.

--- services/rasaConfigGenerator/generateRasa.ts
+++ services/rasaConfigGenerator/generateRasa.ts
@@ -11,7 +11,7 @@ function main() {
     shell.exec(`cp -R ./templates/rasa/* ${rasaPath}`)
     shell.exec(`ls -lah ${rasaPath}`)
     shell.exec(`docker run -v ${rasaPath}:/app -v ${basePath}/models:/app/models rasa/rasa:3.5.6-full train --fixed-model-name ${Customer.id}-${Customer.version}`)
-    shell.exec(`echo "trying to get base models" && ls -lah && ls -lah ${basePath}`)
-    shell.exec(`cd ${basePath}/models/ && scp ${Customer.id}-${Customer.version}.tar.gz eighty@192.168.0.151:/data/rasa/models/`)
+    shell.exec(`echo "trying to get base models" && ls -lah && ls -lah ${basePath}/models`)
+    shell.exec(`cd ${basePath}/models/ && cp ${Customer.id}-${Customer.version}.tar.gz ${process.cwd()}`)
 }
 main()
