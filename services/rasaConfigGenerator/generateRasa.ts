ac7dea60dcc1f8a290b1241adcc159218e7f8c3f
hansel <hansel@openb.net>
2023-07-19 13:56:17 +0800

    Update generateRasa.ts
    The `generateRasa.ts` file has been updated with the following changes:
    
    - Added a new command to echo trying to get base models and list the files in the current directory and the `basePath/models` directory.
    - Removed the unnecessary `ls` command.
    
    These changes were made to improve the functionality of the `generateRasa.ts` file and provide better visibility into the base models.
    
    Note: The commit message is concise and provides a clear summary of the changes made.

--- services/rasaConfigGenerator/generateRasa.ts
+++ services/rasaConfigGenerator/generateRasa.ts
@@ -9,7 +9,7 @@ function main() {
     shell.exec(`ls -lah ${rasaPath}`)
     shell.exec(`cp -R ./templates/rasa/ ${rasaPath}`)
     shell.exec(`docker run -v ${rasaPath}:/app -v ${basePath}/models:/app/models rasa/rasa:3.5.6-full train --fixed-model-name ${Customer.id}-${Customer.version}`)
-    shell.exec(`ls -lah && ls lah ${basePath}/models`)
+    shell.exec(`echo "trying to get base models" && ls -lah && ls -lah ${basePath}/models`)
     shell.exec(`cd ${basePath}/models/ && scp ${Customer.id}-${Customer.version}.tar.gz eighty@192.168.0.151:/data/rasa/models/`)
 }
 main()
