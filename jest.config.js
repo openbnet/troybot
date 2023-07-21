7cab664214188fbb17bcbdc4e23b1c0ee02ed851
hansel <hansel@openb.net>
2023-07-21 08:28:40 +0800

    rasa tests

--- jest.config.js
+++ jest.config.js
@@ -1,26 +1,26 @@
 module.exports = {
-    roots: [
-      "<rootDir>/src",
-    ],
-    testMatch: [
-      "**/__tests__/**/*.+(ts|tsx|js)",
-      "**/?(*.)+(spec|test).+(ts|tsx|js)"
-    ],
-    transform: {
-      "^.+\\.(ts|tsx)$": "ts-jest"
-    },
-    collectCoverageFrom: [
-      "**/*.{js,jsx,ts,tsx}",
-      "!**/*.d.ts",
-      "!**/node_modules/**",
-    ],
-    testTimeout: 5000,
-    "transform": {
-      "^.+\\.tsx?$": [
-        "ts-jest",
-        {
-          "tsconfig": "tsconfig.json"
-        }
-      ]
+  roots: [
+    "<rootDir>/tests",
+  ],
+  testMatch: [
+    "**/__tests__/**/*.+(ts|tsx|js)",
+    "**/?(*.)+(spec|test).+(ts|tsx|js)"
+  ],
+  transform: {
+    "^.+\\.(ts|tsx)$": "ts-jest"
   },
-  }
+  collectCoverageFrom: [
+    "**/*.{js,jsx,ts,tsx}",
+    "!**/*.d.ts",
+    "!**/node_modules/**",
+  ],
+  testTimeout: 5000,
+  "transform": {
+    "^.+\\.tsx?$": [
+      "ts-jest",
+      {
+        "tsconfig": "tsconfig.json"
+      }
+    ]
+  },
+}
--- services/rasaConfigGenerator/generateRasa.ts
+++ services/rasaConfigGenerator/generateRasa.ts
@@ -4,12 +4,12 @@ import shell from "shelljs";
 function main() {
     const basePath = process.cwd() + "/generated"
     const rasaPath = basePath + "/rasa"
-    // console.log("basePath",basePath)
-    // console.log("rasaPath",rasaPath)
-    // generateRasaConfig(rasaPath, Customer)
-    // shell.exec(`ls -lah ${rasaPath}`)
-    // shell.exec(`cp -R ./templates/rasa/* ${rasaPath}`)
-    // shell.exec(`ls -lah ${rasaPath}`)
+    console.log("basePath", basePath)
+    console.log("rasaPath", rasaPath)
+    generateRasaConfig(rasaPath, Customer)
+    shell.exec(`ls -lah ${rasaPath}`)
+    shell.exec(`cp -R ./templates/rasa/* ${rasaPath}`)
+    shell.exec(`ls -lah ${rasaPath}`)
     shell.exec(`docker run -v ${rasaPath}:/app -v ${basePath}/models:/app/models rasa/rasa:3.5.6-full train --fixed-model-name ${Customer.id}-${Customer.version}`)
     shell.exec(`echo "trying to get base models" && ls -lah && ls -lah ${basePath}/models`)
     shell.exec(`cd ${basePath}/models/ && cp ${Customer.id}-${Customer.version}.tar.gz ${process.cwd()}/models`)
