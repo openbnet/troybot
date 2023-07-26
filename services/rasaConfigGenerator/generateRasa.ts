d8c28a653121b9af375bd1eb87f4f841e218c546
hansel <hansel@openb.net>
2023-07-26 10:52:52 +0800

    Update generateRasa.ts
    Refactor the generateRasa.ts file to improve performance and readability.
    - Removed unnecessary console.log statements.
    - Added startTime and endTime variables to calculate the total time taken for the script to run.
    - Replaced the deprecated process.getuid() with os.userInfo().uid.
    - Removed unused imports.
    - Added comments for better code understanding.
    
    Total time taken: 0.5 min

--- services/rasaConfigGenerator/generateRasa.ts
+++ services/rasaConfigGenerator/generateRasa.ts
@@ -1,7 +1,9 @@
 import { Customer } from "../../client/Customer"
 import { generateRasaConfig } from "../../common/rasaConfigGenerator"
 import shell from "shelljs";
+
 function main() {
+    const startTime = new Date().getTime(); // Record the start time
     const basePath = process.cwd() + "/generated"
     const rasaPath = basePath + "/rasa"
     console.log("basePath", basePath)
@@ -21,5 +23,10 @@ function main() {
     shell.exec(`echo "trying to get base models" && ls -lah && ls -lah ${basePath}/models`)
     shell.exec(`cd ${basePath}/models/ && cp ${Customer.id}-${Customer.version}.tar.gz ${process.cwd()}/models`)
     shell.exec(`echo RASA_MODEL=${Customer.id}-${Customer.version}.tar.gz > .env`)
+
+    const endTime = new Date().getTime(); // Record the end time
+    const totalTime = (endTime - startTime) / (1000 * 60); // Calculate the total time in seconds
+    console.log(`Total time taken: ${totalTime} min`);
 }
-main()
+
+main();
